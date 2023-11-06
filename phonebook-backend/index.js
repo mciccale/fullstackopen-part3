496c45223ef5aec47455c76f74356ffd009b9c06
mciccale <marcociccalebaztan@gmail.com>
2023-11-06 23:07:37 +0100

    uibuild

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -13,6 +13,7 @@ const generateRandomId = () => {
   return Math.floor(Math.random() * 1000);
 };
 
+app.use(express.static("dist"));
 app.use(cors());
 app.use(express.json());
 app.use(
@@ -95,7 +96,7 @@ app.delete("/api/persons/:id", (req, res) => {
 });
 
 app.use((_req, res) => {
-  res.status(404).json({ error: "unknown endpoit" });
+  res.status(404).json({ error: "unknown endpoint" });
 });
 
 const PORT = process.env.PORT ?? 3001;
