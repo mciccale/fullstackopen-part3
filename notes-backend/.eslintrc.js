90c93f900959161a38bbf13edb84c1c63720d451
mciccale <marcociccalebaztan@gmail.com>
2023-11-22 16:51:53 +0100

    Notes app restructured

--- notes-backend/.eslintrc.js
+++ notes-backend/.eslintrc.js
@@ -20,11 +20,5 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
   },
-  rules: {
-    eqeqeq: "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", { before: true, after: true }],
-    "no-console": 0,
-  },
+  rules: {},
 };
--- notes-backend/models/note.js
+++ notes-backend/models/note.js
@@ -1,35 +1,20 @@
-const mongoose = require("mongoose");
-
-mongoose.set("strictQuery", false);
-
-const url = process.env.MONGODB_URI;
-
-console.log(`connecting to MongoDB`);
-
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log("connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error(`error connecting to MongoDB: ${err}`);
-  });
+const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
     required: true,
+    minlength: 5,
   },
   important: Boolean,
 });
 
-noteSchema.set("toJSON", {
-  transform: (_doc, obj) => {
-    obj.id = obj._id.toString();
-    delete obj._id;
-    delete obj.__v;
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   },
 });
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema);
